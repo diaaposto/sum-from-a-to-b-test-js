5d7798bc81d70a66c91db24d749f0ecbea2b1df8
Dia Apostolakis <41875537+diaaposto@users.noreply.github.com>
2018-08-30 13:13:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  //if 3 >= 7, return 3
+  if (fromN >= toN) {
+    return fromN;
+  }
+  
+  return fromN + sum((fromN + 1), toN)
+
 }
 
 module.exports = sum;
